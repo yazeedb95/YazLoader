ff4a2e91edc9389f4579347863268fbcc5442079
yazeed <116912266+yazeedb95@users.noreply.github.com>
2023-06-12 20:00:13 +0300

    Update pathFolder.py

--- pathFolder.py
+++ pathFolder.py
@@ -4,7 +4,7 @@ import os
 def path_folder(final_name):    
     downloads_folder = os.path.join(os.path.expanduser("~"), "Downloads")
     # Create the path to the youtube_down folder
-    youtube_down_folder = os.path.join(downloads_folder, "youtube_videos")
+    youtube_down_folder = os.path.join(downloads_folder, "YazLoader")
     
     if not os.path.exists(youtube_down_folder):
     
